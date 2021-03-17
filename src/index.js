1dac82ebc1501debc58fb46a0297528550cbdc93
Igbadumhe <perfectimhetsike.igbadumhe@stanbicibtc.com>
2021-03-17 01:58:01 +0100

    first attenpt at redux

--- src/index.js
+++ src/index.js
@@ -3,6 +3,54 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux';
+
+//Action--a simple function thatr returns an object
+const increment=()=> {
+  return{
+    type:"INCREMENT"
+  }
+}
+
+const decrement=()=> {
+  return {
+    type:"DECREMENT"
+  }
+}
+
+
+//reducer
+const counter=(state=0, action)=> {
+  switch(action.type){
+    case "INCREMENT":
+      return state + 1
+
+    case "DECREMENT":
+      return state - 1;
+  }
+}
+
+
+
+//store
+let store = createStore(counter);
+
+//display to console
+store.subscribe(()=> console.log(store.getState()))
+
+//DISPATCH
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(decrement())
+
+
+
+
+
+
 
 ReactDOM.render(
   <React.StrictMode>
